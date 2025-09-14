66a4988ca888e0d5f2d6e66bcac9cc334081c010
sarithagudur <42064481+sarithagudur@users.noreply.github.com>
2025-09-14 11:38:54 +0100

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,11 @@
+birthday1="15/12/2025"
+birthday2="25/11/2025"
+birthday3="29/12/2024"
+birthday4="22/11/2025"
+birthday5="21/12/2015"
+
+print(birthday5)
+print(birthday4)
+print(birthday3)
+print(birthday2)
+print(birthday1)
