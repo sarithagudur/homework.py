1f79c5815ac4e74dde21f87af7aa75048b584881
sarithagudur <42064481+sarithagudur@users.noreply.github.com>
2025-09-24 17:53:19 +0100

    Add files via upload

---
+++ square_root_homework.py
@@ -0,0 +1,17 @@
+
+# I did not know how to do this can u explain in the next lesson
+
+square=int(input("put your square number here:"))
+
+
+number1=square*100
+number2=(square%100)*50
+number3=(square%100%50)*25
+number4=(square%100%50%25)*5
+
+print("the value of 100 numbers is:", number1)
+print("the value of 50 numbers is:", number2)
+print("the value of 25 numbers is:", number3)
+print("the value of 5 numbners is:", number4)
+
+
